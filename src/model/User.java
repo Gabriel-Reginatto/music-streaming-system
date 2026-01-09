f7753732439569faa53fb6f47fa82ec47dcab0c8
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-09 15:57:05 -0300

    Implement addplaylist method to User

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String email;
+    private List<PlayList> playLists;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.playLists = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void addplayList(PlayList playList) {
+        if (playList == null) {
+            throw new IllegalArgumentException("Playlist cannot be null");
+        }
+        playLists.add(playList);
+    }
 }
