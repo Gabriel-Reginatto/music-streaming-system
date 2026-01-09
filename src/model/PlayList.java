50fd98e3cea7b4b2b82eaa2947173093f1c6d2e6
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-09 15:06:39 -0300

    Add media managment and duration calculation to Playlist

--- src/model/PlayList.java
+++ src/model/PlayList.java
@@ -1,4 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PlayList {
+    private String name;
+    private List<Media> playlistMedia;
+
+    public PlayList(String name) {
+        this.name = name;
+        this.playlistMedia = new ArrayList<>();
+    }
+
+    public void addMedia(Media media) {
+        if (media == null) {
+            throw new IllegalArgumentException("Media cannot be null");
+        }
+        playlistMedia.add(media);
+    }
+
+    public List<Media> getPlaylistMedia() {
+        return Collections.unmodifiableList(playlistMedia);
+    }
+
+    public int getTotalDuration() {
+        int total = 0;
+
+        for(Media media : playlistMedia) {
+            total += media.getDuration();
+        }
+
+        return total;
+    }
 }
