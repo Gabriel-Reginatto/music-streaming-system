5bb72ad318ea7bbf97abd4720097d4b17b388216
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-09 15:33:17 -0300

    Add media method to catalog

--- src/service/Catalog.java
+++ src/service/Catalog.java
@@ -1,4 +1,21 @@
 package service;
 
+import model.Media;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalog {
+    private List<Media> mediaList;
+
+    public Catalog() {
+        this.mediaList = new ArrayList<>();
+    }
+
+    public void addMedia(Media media) {
+        if (media == null) {
+            throw new IllegalArgumentException("Media cannot be null");
+        }
+        mediaList.add(media);
+    }
 }
