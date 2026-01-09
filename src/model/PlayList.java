072ff9fbbe62fbae85b96c53c0309c80a8d06d10
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-09 15:26:13 -0300

    Finalize Playlist class

--- src/model/PlayList.java
+++ src/model/PlayList.java
@@ -6,31 +6,52 @@ import java.util.List;
 
 public class PlayList {
     private String name;
-    private List<Media> playlistMedia;
+    private List<Media> medias;
 
     public PlayList(String name) {
         this.name = name;
-        this.playlistMedia = new ArrayList<>();
+        this.medias = new ArrayList<>();
     }
 
     public void addMedia(Media media) {
         if (media == null) {
             throw new IllegalArgumentException("Media cannot be null");
         }
-        playlistMedia.add(media);
+        medias.add(media);
     }
 
-    public List<Media> getPlaylistMedia() {
-        return Collections.unmodifiableList(playlistMedia);
+    public boolean removeMedia(Media media) {
+        if (media == null) {
+            throw new IllegalArgumentException("Media cannot be null");
+        }
+        return medias.remove(media);
     }
 
     public int getTotalDuration() {
         int total = 0;
 
-        for(Media media : playlistMedia) {
+        for(Media media : medias) {
             total += media.getDuration();
         }
 
         return total;
+    }
+
+    public List<Media> getMedias() {
+        return Collections.unmodifiableList(medias);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEmpty() {
+        return medias.isEmpty();
+    }
+    @Override
+    public String toString() {
+        return "Playlist: " + name +
+                "Media: " + medias.size() +
+                "Total duration: " + getTotalDuration();
     }
 }
