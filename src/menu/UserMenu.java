3a178d63a54cacb2a0e2ba89c99e6df07a0f4c1d
Gabriel Reginatto <reginattogb@gmail.com>
2026-01-09 21:50:22 -0300

    Add createUsedr method to MenuUser with duplicate email check

--- src/menu/UserMenu.java
+++ src/menu/UserMenu.java
@@ -1,4 +1,38 @@
 package menu;
 
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserMenu {
+    private List<User> users;
+    private Scanner  scanner;
+
+ public UserMenu() {
+     this.users = new ArrayList<>();
+     this.scanner = new Scanner(System.in);
+ }
+
+ public void createUser() {
+     System.out.println("=== Criar nova conta ===");
+
+     System.out.println("Digite seu nome: ");
+     String name = scanner.nextLine();
+
+     System.out.println("Digite seu email: ");
+     String email = scanner.nextLine();
+
+     User newUser = new User(name, email);
+
+     if (users.contains(newUser)) {
+         System.out.println("Erro: já existe um usuário com esse email!");
+         return;
+     }
+
+     users.add(newUser);
+     System.out.println("Conta criada com sucesso!");
+ }
+
 }
